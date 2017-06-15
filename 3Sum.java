fab01a03ab4f7f3c911aa752b3cee1df2566ef4a
专注而高效 <1206557575@qq.com>
2017-06-15 16:04:34 +0800

    Update 3Sum.java

--- 3Sum.java
+++ 3Sum.java
@@ -1,30 +1,36 @@
-
-import java.util.*; 
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        
         List<List<Integer>> anss=new ArrayList();
         if(nums.length<3){return anss;}
-        int max=0;
-        int m=nums.length;
-        HashSet<List> va = new HashSet<List>();  
-        for(int i=0;i<m;i++){
-            for(int j=i+1;j<m;j++){
-              int[] left=(Arrays.copyOfRange(nums, j+1, m));
-              Arrays.sort(left);
-              int ok=Arrays.binarySearch(left,0-nums[i]-nums[j]);
-              //if(nums[i]==-4&&nums[j]==3){ anss.add(new ArrayList<Integer>(){{add(-4);add(3);add(ok);}});}
-              if(ok>=0){
-                 List<Integer> ans=new ArrayList<Integer>();
-                 ans.add(nums[i]);ans.add(nums[j]); ans.add(0-nums[i]-nums[j]);
-                 Collections.sort(ans);
-                 if(!va.contains(ans)){
-                 va.add(ans);
-                 anss.add(ans);
-                 } 
-                 
-              }
-        }
-        }
+        
+        Arrays.sort(nums);
+        int k=nums.length-1;
+        
+        for(int i=0;i<nums.length;i++){
+            if(i>0&&nums[i]==nums[i-1])continue;
+            int j=i+1;
+            int right=k;
+            //HashSet<List> va = new HashSet<List>();
+            while(j<right){
+                int left=0-nums[i]-nums[j];
+                if(left<nums[right]){
+                    right--;
+                }else if(left==nums[right]){
+            
+                     List<Integer> ans=new ArrayList<Integer>();
+                     ans.add(nums[i]); ans.add(nums[j]); ans.add(nums[right]);
+                     anss.add(ans);
+                     right--;
+                     j++;
+                     while(j<right&&nums[j]==nums[j-1])j++;
+                     while(j<right&&nums[right]==nums[right+1])right--;
+                }else{
+                    j++;
+                }
+            }
+        }//for 
         return anss;
+        
     }
 }
