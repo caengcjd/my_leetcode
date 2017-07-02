0ddf649d2c209af2f7b8bb7b52d13f1009032a81
专注而高效 <1206557575@qq.com>
2017-07-02 15:02:06 +0800

    Update Jump Game.java

--- Jump Game.java
+++ Jump Game.java
@@ -22,3 +22,27 @@ public class Solution {
 }
 
 //数量级别一多就不行了额
+
+
+
+public class Solution {
+    public boolean canJump(int[] nums) {
+        
+        if(nums.length==0) return false;
+        int local=nums[0];
+        int global=nums[0];
+        
+        for(int i=1;i<=nums.length-1&&i<=global;i++){
+            
+            local=Math.max(i+nums[i],local);
+            global=Math.max(global,local);
+            
+        }
+        if(global<(nums.length-1)){
+            return false;
+        }else{
+        return true;
+        }
+        
+    }
+}
